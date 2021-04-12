29e7da6ca757016d25f1a3ffa8d230d6f8d02ba6
Marco Lopes <marcoalopes@outlook.com>
2021-04-12 19:29:15 -0300

    adição da função para pegar os valores do contato

--- KodEletronics/src/app/contato/contato.component.ts
+++ KodEletronics/src/app/contato/contato.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-contato',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContatoComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor(private http: HttpClient) { }
+  url = "http://localhost:3001/contact/"
+
+  // criar(dataContactServer: []): Observable<[]> {
+  //   return this.http.post<[]>(this.url, dataContactServer)
+  // }
+
 
   ngOnInit(): void {
   }
+
+  // dataContactServer = []
+  newObject = [{
+    "nome": "",
+    "email": "",
+    "mensagem": ""
+  }]
+
+
+  getContact(): Observable<[]> {
+    var nome = (<HTMLSelectElement>document.getElementById('nameText')).value
+    var email = (<HTMLSelectElement>document.getElementById('emailText')).value
+    var message = (<HTMLSelectElement>document.getElementById('contactText')).value
+    var dataContact = [{
+      "nome": nome,
+      "email": email,
+      "mensagem": message
+    }]
+    
+    console.log(dataContact)
+    return this.http.post<[]>(this.url, dataContact)
+  }
+
+
+
+
 
 }
