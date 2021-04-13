22ef30308e7df4536212e463dcc5b8e6c75064c5
Marco Lopes <marcoalopes@outlook.com>
2021-04-13 13:51:37 -0300

    criação do serviço contato para consumir a API do JSON

--- KodEletronics/src/app/contato/contato.component.ts
+++ KodEletronics/src/app/contato/contato.component.ts
@@ -1,6 +1,10 @@
 import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+// import { Observable } from 'rxjs';
+import { ContatoService } from '../services/contato.service';
+import { Contato } from '../models/contatoModel'
+// import { Observable } from 'rxjs/internal/observable'
+
 
 @Component({
   selector: 'app-contato',
@@ -9,43 +13,33 @@ import { Observable } from 'rxjs';
 })
 export class ContatoComponent implements OnInit {
 
+  contato: Contato;
 
+  constructor(private service: ContatoService) { 
+    this.contato = {} as Contato;
+  }
 
-  constructor(private http: HttpClient) { }
-  url = "http://localhost:3001/contact/"
-
-  // criar(dataContactServer: []): Observable<[]> {
-  //   return this.http.post<[]>(this.url, dataContactServer)
-  // }
 
 
   ngOnInit(): void {
   }
 
-  // dataContactServer = []
-  newObject = [{
-    "nome": "",
-    "email": "",
-    "mensagem": ""
-  }]
 
 
-  getContact(): Observable<[]> {
-    var nome = (<HTMLSelectElement>document.getElementById('nameText')).value
-    var email = (<HTMLSelectElement>document.getElementById('emailText')).value
-    var message = (<HTMLSelectElement>document.getElementById('contactText')).value
-    var dataContact = [{
-      "nome": nome,
-      "email": email,
-      "mensagem": message
-    }]
-    
-    console.log(dataContact)
-    return this.http.post<[]>(this.url, dataContact)
+  getContact(contato:Contato) {
+    contato.nome = (<HTMLSelectElement>document.getElementById('nameText')).value
+    contato.email = (<HTMLSelectElement>document.getElementById('emailText')).value
+    contato.mensagem = (<HTMLSelectElement>document.getElementById('contactText')).value
+
+
+    this.service.salvar(contato).subscribe(() => {
+      console.log("contato salvo");
+    })
   }
-
-
-
-
-
 }
+
+
+
+
+
+
---
+++ KodEletronics/src/app/services/contato.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs'
+// import { observable, Observable } from 'rxjs';
+import { Contato } from '../models/contatoModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContatoService {
+
+  constructor(private http: HttpClient) { }
+  url = "http://localhost:3001/contact"
+
+  listar(): Observable<Contato[]> {
+    return this.http.get<Contato[]>(this.url);
+  }
+
+  buscarPorId(id: number): Observable<Contato>{
+    const path = `${this.url}/${id}`;
+    return this.http.get<Contato>(path);
+  }
+
+  salvar(contato: Contato): Observable<Contato> {
+    return this.http.post<Contato>(this.url, contato)
+  }
+
+  atualizar(contato: Contato): Observable<Contato>{
+    const path = `${this.url}/${contato.id}`;
+    return this.http.put<Contato>(path, contato)
+  }
+
+}
